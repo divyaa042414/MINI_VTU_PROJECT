35a91a0a0c5c961546921f1e5b544921bd6aef65
Divya S <divya042414@gmail.com>
2024-02-08 00:27:26 +0530

    Added vision mission n mandate

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Carousel} from 'react-bootstrap';
+import {Carousel,Row,Col,Button} from 'react-bootstrap';
 
 const HomeScreen = () => {
 
@@ -44,7 +44,32 @@ const HomeScreen = () => {
                     src="Images/img5.jpg"
                     alt="Third slide"/>
                   </Carousel.Item>
-            </Carousel>
+            </Carousel><br/>
+
+            <div>
+                <Row>
+                    <Col md={4} sm={12}>
+                    <h3><a href="#">Vision</a></h3>
+                    <p className="p-style">To become an outstanding Technological University at the cutting edge of Science and Technology that produces world class Knowledge-delivery, Research, Extension and Leadership in Technology innovation for Industry and Society.</p>
+                    <p className="p-style">To plan the development of technical education, to establish value-based and need-based education and training in engineering and technology, with a view to generate qualified and competent manpower, responsive to technological and societal needs.</p>
+                    <Button variant="warning">Read More</Button>
+                    </Col>
+
+                    <Col md={4} sm={12}>
+                    <h3><a href="#">Mission</a></h3>
+                    <p className="p-style">To plan the development of technical education, to establish value-based and need-based education and training in engineering and technology, with a view to generate qualified and competent manpower, responsive to technological and societal needs.</p>
+                    <p className="p-style">To plan the development of technical education, to establish value-based and need-based education and training in engineering and technology, with a view to generate qualified and competent manpower, responsive to technological and societal needs.</p>
+                    <Button variant="warning">Read More</Button>
+                    </Col>
+
+                    <Col md={4} sm={12}>
+                    <h3><a href="#">Mandate</a></h3>
+                    <p className="p-style">The Visvesvaraya Technological University has been established by the Government of the Karnataka in order to Promote planned and sustainable development of technical education consistent with state and national policies.</p>
+                    <p className="p-style">To plan the development of technical education, to establish value-based and need-based education and training in engineering and technology, with a view to generate qualified and competent manpower, responsive to technological and societal needs.</p>
+                    <Button variant="warning">Read More</Button>
+                    </Col>
+                </Row>
+            </div>
         
        </div>
       </>
